e8233598d1652d156ee2143135b48de00161968e
Nejdet Dalaslan <nejdet@initialcode.com.tr>
2023-12-27 03:26:16 +0300

    prettier and eslint were installed

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['universe', 'universe/native'],
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+  },
+};
