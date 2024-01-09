fe820695040cfa73e0fad7b39bf1dbb7a7303411
Nejdet Dalaslan <nejdet@initialcode.com.tr>
2024-01-10 00:09:46 +0300

    - Enums' comments were added

--- src/db/Enums.ts
+++ src/db/Enums.ts
@@ -1,9 +1,16 @@
+/**
+ * Each filter represents a different search criterion.
+ */
 export const enum Filters {
   Author = 'Author',
   BookName = 'Bookname',
   ISBN = 'ISBN',
 }
 
+/**
+ * Enum defining different types of users.
+ * Each user type represents a specific role for users.
+ */
 export const enum UserTypes {
   Admin = 'Admin',
   Guest = 'Guest',
